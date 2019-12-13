49f843f29824cc21ae0eedb532770d05a3e8f844
thelentanguy <thetanguy@icloud.com>
2019-12-13 15:30:57 +0100

    base ml + update slider
    -mention légales base
    - slider update

--- caroussel.js
+++ caroussel.js
@@ -1,4 +1,6 @@
+/*
 $('.slider').slick({
+    dots: true,
     centerMode: true,
     centerPadding: '60px',
     slidesToShow: 3,
@@ -21,5 +23,38 @@ $('.slider').slick({
           slidesToShow: 1
         }
       }
+    ]
+  });
+*/
+  $('.slider').slick({
+    slidesToShow: 2,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
     ]
   });
