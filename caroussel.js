9a181b2f8fa0e7f056f5f9f48f041ef359322969
thelentanguy <thetanguy@icloud.com>
2019-12-17 22:30:41 +0100

    debug
    trop tard pour ca

---
+++ caroussel.js
@@ -0,0 +1,27 @@
+  $('.slider').slick({
+    slidesToShow: 2,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
